395efd143670becc65ad9e0d015c37228262908c
Sam McManus <samuelmcmanus819@gmail.com>
2023-06-04 00:44:37 -0500

    Add basic windows home dir detection

---
+++ detectdir.go
@@ -0,0 +1,26 @@
+package detectdir
+
+import (
+	"errors"
+	"os"
+	"os/user"
+	"runtime"
+)
+
+func Homedir() (string, error) {
+	if runtime.GOOS == "windows" {
+		if currentUser, err := user.Current(); err != nil || currentUser.HomeDir != "" {
+			return currentUser.HomeDir, nil
+		}
+		if homeDir := os.Getenv("USERPROFILE"); homeDir != "" {
+			return homeDir, nil
+		}
+		if homeDrive := os.Getenv("HOMEDRIVE"); homeDrive != "" {
+			if homeDir := os.Getenv("HOMEPATH"); homeDir != "" {
+				return homeDrive + homeDir, nil
+			}
+		}
+		return "", errors.New("Failed to obtain home directory")
+	}
+	return "", nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/samuelmcmanus819/directory-detector
+
+go 1.20
+
+require golang.org/x/sys v0.8.0
